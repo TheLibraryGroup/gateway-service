6e927b78c10c21a64bbd1c37f70fc47713a345ff
thibaut54 <john_milton100@hotmail.com>
2020-03-19 21:47:14 +0100

    remove keycloak

--- src/main/java/org/thibaut/servicegateway/GatewayApplication.java
+++ src/main/java/org/thibaut/servicegateway/GatewayApplication.java
@@ -5,23 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class GatewayApplication {
 
 //	@Bean
-//	RouteLocator routeLocator( RouteLocatorBuilder routeLocatorBuilder ){
-//		return routeLocatorBuilder.routes()
-//					.route( route->route.path( "/api/books/**" ).uri( "lb://thelibrary-microservice-catalog" ).id( "catalog-books" ) )
+//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				       .route(route -> route
+//						                   .path("/api/books/**")
+//						                   .filters(f -> f.hystrix(config -> config.setName("d").setFallbackUri( "forward:/defaultBook"  )))
+//						                   .uri("lb://thelibrary-ms-book")
+//					                       .id( "ms-books" ))
 //				.build();
 //	}
 
 	@Bean
-	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator( ReactiveDiscoveryClient reactiveDiscoveryClient,
-	                                                                             DiscoveryLocatorProperties discoveryLocatorProperties ){
+	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+			ReactiveDiscoveryClient reactiveDiscoveryClient,
+			DiscoveryLocatorProperties discoveryLocatorProperties ){
 		return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, discoveryLocatorProperties);
 	}
 
