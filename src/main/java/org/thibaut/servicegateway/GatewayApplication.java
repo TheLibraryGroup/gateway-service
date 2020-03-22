c6ad12a8d17e544f685e9e505dabcef9e7f54640
thibaut54 <john_milton100@hotmail.com>
2020-03-23 00:54:21 +0100

    try add cors, doesn't work

--- src/main/java/org/thibaut/servicegateway/GatewayApplication.java
+++ src/main/java/org/thibaut/servicegateway/GatewayApplication.java
@@ -9,8 +9,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin("*")
 public class GatewayApplication {
 
 //	@Bean
