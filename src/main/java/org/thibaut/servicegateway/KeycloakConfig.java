6cc0e59fda53942eb128c8b2b5ec02f31b52d867
thibaut54 <john_milton100@hotmail.com>
2020-03-22 22:28:19 +0100

    CORS error + doesn't send jwt to MS

--- src/main/java/org/thibaut/servicegateway/KeycloakConfig.java
+++ src/main/java/org/thibaut/servicegateway/KeycloakConfig.java
@@ -2,9 +2,11 @@ package org.thibaut.servicegateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @Configuration
+@CrossOrigin("*")
 public class KeycloakConfig {
 
 //	@Bean
--- src/main/java/org/thibaut/servicegateway/SecurityConfig.java
+++ src/main/java/org/thibaut/servicegateway/SecurityConfig.java
@@ -1,14 +1,23 @@
 package org.thibaut.servicegateway;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 //@Configuration
 @EnableWebFluxSecurity
+@AllArgsConstructor
+//@CrossOrigin("*")
 public class SecurityConfig {
 
 //	@Bean
@@ -32,6 +41,7 @@ public class SecurityConfig {
 	@Bean
 	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
 		http
+				.cors(  ).configurationSource(configurationSource()).and()
 				.authorizeExchange()
 				.anyExchange().permitAll()
 //				.pathMatchers("/api/editor/**").authenticated()
@@ -40,6 +50,19 @@ public class SecurityConfig {
 				.oauth2ResourceServer()
 				.jwt();
 		return http.build();
+	}
+
+	private CorsConfigurationSource configurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.setAllowCredentials(true);
+		config.addAllowedHeader("X-Requested-With");
+		config.addAllowedHeader("Content-Type");
+		config.addAllowedMethod( HttpMethod.POST);
+		config.addAllowedMethod( HttpMethod.GET);
+		source.registerCorsConfiguration("/logout", config);
+		return source;
 	}
 
 
