34e2b5cf4fe505cfd6b202bb46179f77520c6361
thibaut54 <john_milton100@hotmail.com>
2020-03-22 23:40:10 +0100

    doesnt compile

--- src/main/java/org/thibaut/servicegateway/SecurityConfig.java
+++ src/main/java/org/thibaut/servicegateway/SecurityConfig.java
@@ -1,69 +1,50 @@
 package org.thibaut.servicegateway;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-//@Configuration
-@EnableWebFluxSecurity
+@Configuration
+@EnableWebSecurity
 @AllArgsConstructor
-//@CrossOrigin("*")
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//	@Bean
-//	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
-//	                                                        ReactiveClientRegistrationRepository clientRegistrationRepository) {
-//		// Authenticate through configured OpenID Provider
-////		http.oauth2Login();
-////		// Also logout at the OpenID Connect provider
-////		http.logout(logout -> logout.logoutSuccessHandler(new OidcClientInitiatedServerLogoutSuccessHandler(
-////				clientRegistrationRepository)));
-//		// Require authentication for all requests
-////		http.authorizeExchange().anyExchange().authenticated();
-//		// Allow showing /home within a frame
-//		http.authorizeExchange().pathMatchers( "http://localhost:8081/THELIBRARY-MS-BOOK/api/book/**" ).permitAll();
-//		http.headers().frameOptions().mode( XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
-//		// Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
-//		http.csrf().disable();
-//		return http.build();
-//	}
+//	private final JwtRequestFilter jwtRequestFilter;
+
+	@Override
+	protected void configure( HttpSecurity http) throws Exception {
+		// Validate tokens through configured OpenID Provider
+		http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+		http.cors().and().authorizeRequests().mvcMatchers("/api/books").hasRole("admin");
+		// Require authentication for all requests
+//		http.authorizeRequests().anyRequest().authenticated();
+		// Allow showing pages within a frame
+		http.headers().frameOptions().sameOrigin();
+	}
+
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		// Convert realm_access.roles claims to granted authorities, for use in access decisions
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+		return jwtAuthenticationConverter;
+	}
 
 	@Bean
-	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-		http
-				.cors(  ).configurationSource(configurationSource()).and()
-				.authorizeExchange()
-				.anyExchange().permitAll()
-//				.pathMatchers("/api/editor/**").authenticated()
-//				.pathMatchers("/api/book/**").permitAll()
-				.and()
-				.oauth2ResourceServer()
-				.jwt();
-		return http.build();
+	public JwtDecoder jwtDecoderByIssuerUri( OAuth2ResourceServerProperties properties) {
+		String issuerUri = properties.getJwt().getIssuerUri();
+		NimbusJwtDecoder jwtDecoder = ( NimbusJwtDecoder ) JwtDecoders.fromIssuerLocation(issuerUri);
+		// Use preferred_username from claims as authentication name, instead of UUID subject
+		jwtDecoder.setClaimSetConverter(new UsernameSubClaimAdapter());
+		return jwtDecoder;
 	}
-
-	private CorsConfigurationSource configurationSource() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOrigin("*");
-		config.setAllowCredentials(true);
-		config.addAllowedHeader("X-Requested-With");
-		config.addAllowedHeader("Content-Type");
-		config.addAllowedMethod( HttpMethod.POST);
-		config.addAllowedMethod( HttpMethod.GET);
-		source.registerCorsConfiguration("/logout", config);
-		return source;
-	}
-
 
 }
