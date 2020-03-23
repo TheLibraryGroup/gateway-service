fa5e61b6ccce13f86ee79457b73504cb9d4780dd
JMilton <john_milton100@hotmail.com>
2020-03-23 14:42:29 +0100

    seems to work with postman test

--- src/main/java/org/thibaut/servicegateway/GatewayApplication.java
+++ src/main/java/org/thibaut/servicegateway/GatewayApplication.java
@@ -9,6 +9,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
@@ -26,15 +30,36 @@ public class GatewayApplication {
 //				.build();
 //	}
 
-	@Bean
-	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
-			ReactiveDiscoveryClient reactiveDiscoveryClient,
-			DiscoveryLocatorProperties discoveryLocatorProperties ){
-		return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, discoveryLocatorProperties);
-	}
+//	@Bean
+//	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+//			ReactiveDiscoveryClient reactiveDiscoveryClient,
+//			DiscoveryLocatorProperties discoveryLocatorProperties ){
+//		return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, discoveryLocatorProperties);
+//	}
 
 	public static void main( String[] args ) {
 		SpringApplication.run( GatewayApplication.class, args );
+	}
+
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain( ServerHttpSecurity http,
+	                                                         ReactiveClientRegistrationRepository clientRegistrationRepository) {
+		// Authenticate through configured OpenID Provider
+//		http.oauth2Login();
+//
+//		// Also logout at the OpenID Connect provider
+//		http.logout(logout -> logout.logoutSuccessHandler(
+//				new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
+
+		// Require authentication for all requests
+		http.cors().and().authorizeExchange().anyExchange().permitAll();
+
+		// Allow showing /home within a frame
+		http.headers().frameOptions().mode( XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
+
+		// Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
+		http.csrf().disable();
+		return http.build();
 	}
 
 }
