c878920dec9b77d8911f0422cbadebd44ba2ac20
thibaut54 <john_milton100@hotmail.com>
2020-03-25 05:30:04 +0100

    fix<cors> add custom CorsWebFilter worked!

--- src/main/java/org/thibaut/servicegateway/GatewayApplication.java
+++ src/main/java/org/thibaut/servicegateway/GatewayApplication.java
@@ -1,9 +1,11 @@
 package org.thibaut.servicegateway;
 
 import lombok.AllArgsConstructor;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
@@ -12,6 +14,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
@@ -19,14 +22,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.filter.CorsFilter;
 
 import java.io.ObjectInputStream;
 
 @SpringBootApplication
-@CrossOrigin("*")
 public class GatewayApplication {
-
-//	private final JwtRequestFilter jwtRequestFilter;
 
 //	@Autowired
 //	private TokenRelayGatewayFilterFactory filterFactory;
@@ -43,12 +44,12 @@ public class GatewayApplication {
 //				.build();
 //	}
 
-//	@Bean
-//	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
-//			ReactiveDiscoveryClient reactiveDiscoveryClient,
-//			DiscoveryLocatorProperties discoveryLocatorProperties ){
-//		return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, discoveryLocatorProperties);
-//	}
+	@Bean
+	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+			ReactiveDiscoveryClient reactiveDiscoveryClient,
+			DiscoveryLocatorProperties discoveryLocatorProperties ){
+		return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, discoveryLocatorProperties);
+	}
 
 	public static void main( String[] args ) {
 		SpringApplication.run( GatewayApplication.class, args );
@@ -57,22 +58,15 @@ public class GatewayApplication {
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain( ServerHttpSecurity http,
 	                                                         ReactiveClientRegistrationRepository clientRegistrationRepository) {
-		// Authenticate through configured OpenID Provider
-//		http.oauth2Login();
-//
-//		// Also logout at the OpenID Connect provider
-//		http.logout(logout -> logout.logoutSuccessHandler(
-//				new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
 
 		// Require authentication for all requests
 		http.cors().and().authorizeExchange().anyExchange().permitAll();
+
 		// Allow showing /home within a frame
 //		http.headers().frameOptions().mode( XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
 
 		// Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
-//		http.csrf().disable();
-//		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		http.csrf().disable();
 		return http.build();
 	}
-
 }
