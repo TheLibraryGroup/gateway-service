f8572afdb98c165d52633cd6b17e8db5cbf5ad34
JMilton <john_milton100@hotmail.com>
2022-02-15 18:39:05 +0100

    add work in progress

--- src/main/java/org/thibaut/servicegateway/SecurityConfig.java
+++ src/main/java/org/thibaut/servicegateway/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.thibaut.servicegateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,8 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 
 @Configuration
 public class SecurityConfig  {
@@ -22,16 +25,19 @@ public class SecurityConfig  {
 			ReactiveClientRegistrationRepository clientRegistrationRepository) throws Exception {
 
 		// Require authentication for all requests
-		http.cors().and().authorizeExchange().pathMatchers( HttpMethod.GET, "/assets/**" ).permitAll();
-
 		http.cors().and().authorizeExchange().anyExchange().permitAll();
 
 		// Allow showing /home within a frame
-		http.headers().frameOptions().mode( XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
+		http.headers().frameOptions().mode( XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN );
 
 		// Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
 		http.csrf().disable();
 
 		return http.build();
 	}
+
+//	@Bean
+//	RouterFunction staticResourceLocator(){
+//		return RouterFunctions.resources("/css/**", new ClassPathResource("ui/"));
+//	}
 }
